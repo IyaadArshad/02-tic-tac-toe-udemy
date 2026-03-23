b195a315d7794382f203ed0872f23c24522a63e4
IyaadArshad <hello@iyaadarshad.com>
2026-03-23 21:03:08 +0530

    updates for lecture 76

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,24 @@
-export default function Player({ playerName, playerSymbol}) {
+import { useState } from "react";
+
+export default function Player({ playerName, playerSymbol }) {
+
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEdit() {
+        if (isEditing)
+            setIsEditing(false);
+        else
+            setIsEditing(true);
+    }
+
     return (
         <li>
             <span className="player">
-                <span className="player-name">{playerName}</span>
+                {!isEditing ? <span className="player-name">{playerName}</span>
+                    : <input type="text" required></input>}
                 <span className="player-symbol">{playerSymbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={handleEdit}>{isEditing ? "Save" : "Edit"}</button>
         </li>
     );
 }
