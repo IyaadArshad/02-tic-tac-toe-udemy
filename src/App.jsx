97d43dbb6df04d6c9f0913418145540130e50b2f
IyaadArshad <hello@iyaadarshad.com>
2026-03-23 20:48:37 +0530

    updates for lecture 74

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 
 function App() {
-  
+
 
   return (
-    <p>coming soon</p>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player A</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player B</span>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+        PLAYERS
+
+        GAME BOARD
+      </div>
+
+      LOG
+    </main>
   )
 }
 
