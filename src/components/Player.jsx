217b00caacd18793adb994f7b67fdf3933da1702
IyaadArshad <hello@iyaadarshad.com>
2026-03-23 21:07:02 +0530

    updates for lecture 78

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,18 +4,17 @@ export default function Player({ playerName, playerSymbol }) {
 
     const [isEditing, setIsEditing] = useState(false);
 
+    let name = playerName;
+
     function handleEdit() {
-        if (isEditing)
-            setIsEditing(false);
-        else
-            setIsEditing(true);
+        setIsEditing(!isEditing)
     }
 
     return (
         <li>
             <span className="player">
                 {!isEditing ? <span className="player-name">{playerName}</span>
-                    : <input type="text" required></input>}
+                    : <input type="text" value={name} required></input>}
                 <span className="player-symbol">{playerSymbol}</span>
             </span>
             <button onClick={handleEdit}>{isEditing ? "Save" : "Edit"}</button>
